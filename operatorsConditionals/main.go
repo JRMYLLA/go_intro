073904ce1270968f5f6e492226a0c3cb10dbe660
Jerry <kapu12f@gmail.com>
2025-07-02 09:33:37 -0500

    Actualice el archivo con los casos de condicionales.

--- operatorsConditionals/main.go
+++ operatorsConditionals/main.go
@@ -41,4 +41,60 @@ func main() {
 
 	fmt.Println(yearsOld<25 && yearsOld==32|| yearsOld <40)// (false and true) or (true) -> true
 	fmt.Println(yearsOld<25 && yearsOld==32|| yearsOld <30)// (false and true) or (false) -> false
+
+
+	fmt.Println("------------------------")
+	//Condicionales in Go
+	yearsOld1 := 20
+	if yearsOld1 > 28 {
+		fmt.Printf("%d is higher than 18\n", yearsOld1)
+	}
+
+	boolVar := true
+	if boolVar {
+		fmt.Println("Is true")
+	} else {
+		fmt.Println("Is false")
+	}
+
+	if value :=true; value == true{
+		fmt.Println("is true")
+	}
+	// various conditionals
+	number := 10
+	if number == 1 {
+		fmt.Println("one")
+	} else if number == 2 {
+		fmt.Println("two")
+	}else if number == 3 {
+		fmt.Println("three")
+	} else {
+		fmt.Println("other number")
+	}
+
+	//other conditional is switch
+	switch number {
+		case 1:
+			fmt.Println("one")
+		case 2:
+			fmt.Println("two")
+		case 3:
+			fmt.Println("three")
+		default:
+			fmt.Println("other number")
+
+
+
+
+}
+// switch with multiple cases
+
+switch{
+case number > 0:
+	fmt.Println("positive number")
+case number < 0:
+	fmt.Println("negative number")
+	case number == 0:
+	fmt.Println("zero")
+}
 }
