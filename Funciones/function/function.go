5715a43deedaaac377f6403a7e5e02012902a438
Jerry <kapu12f@gmail.com>
2025-07-06 22:31:15 -0500

    Functionss

--- Funciones/function/function.go
+++ Funciones/function/function.go
@@ -1,6 +1,9 @@
 package function
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Operation int
 
@@ -21,20 +24,63 @@ func RepeatString(increment int, value string) {
 		fmt.Print(value)
 	}
 }
-func Calc(op Operation, x, y float64)(float64, bool){
+func Calc(op Operation, x, y float64)(float64, error){
 
 	switch op{
 		case SUM:
-			return x + y, false
+			return x + y, nil
 		case SUB:
-			return x - y, false
+			return x - y, nil
 		case DIV:
 			if y == 0{
-				return 0, true
+				return 0, errors.New("y mustn't be zero")
 			}
-			return x / y, false
+			return x / y, nil
 		case MUL:
-			return x * y, false
+			return x * y, nil
 	}
-	return 0, true
+	return 0, errors.New("Has been an error")
 }
+
+func Split(V int) (x,y int) {
+	// This function splits an integer into two parts
+	// For example, if v is 10, it returns 5 and 5
+	// If v is odd, it returns the floor division and the ceiling division
+	x = V * 4 / 9
+	y = V - x
+	return 
+}
+
+func MSum(values ...float64) float64 {
+	var sum float64
+	for _,v:= range values {
+		sum += v
+	}
+	return sum
+}
+
+func MOperations(op Operation, values ...float64) (float64, error) {
+	if len(values) == 0 {
+		return 0, errors.New("No values provided")
+	}
+	sum := values[0]
+	for _, v := range values[1:] {
+		switch op {
+		case SUM:
+			sum += v
+		case SUB:
+			sum -= v
+		case DIV:
+			if v == 0 {
+				return 0, errors.New("Division by zero")
+			}
+			sum /= v
+		case MUL:
+			sum *= v
+		default:
+			return 0, errors.New("Invalid operation")	
+		}
+	}
+	return sum, nil
+}
+
--- Funciones/main.go
+++ Funciones/main.go
@@ -19,9 +19,37 @@ func main() {
 	// Using the Calc function from the function package
 
 	v, err := function.Calc(function.SUM, 3,6)
-	fmt.Println("Value:", v, " - Error:", err)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+	fmt.Println("Value: ", v)
+	}
 	v, err = function.Calc(function.DIV, 3,0)
-	fmt.Println("Value: ", v, " - Error: ", err)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Value: ", v)
+	}
+
+	x, y := function.Split(20)
+	fmt.Println("Value 1: ", x, "Value 2: ", y)
+	fmt.Println()
+
+	v=function.MSum(23,12,32,12,3,1,2,3,2,1,23,12,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+	fmt.Println("multy sum: ", v)
+
+	fmt.Println("------------------------------")
+
+
+	v, err = function.MOperations(function.SUM, 1,2,3,4,5,6,7,8,9,10)
+	fmt.Println("multy sum: ", v, "Error: ", err)
+	v, err = function.MOperations(function.DIV, 1,2,3,4,5,6,7,8,9,10)
+	fmt.Println("multy div: ", v, "Error: ", err)
+	v, err = function.MOperations(function.MUL, 1,2,3,4,5,6,7,8,9,10)
+	fmt.Println("multy mul: ", v, "Error: ", err)
+	v, err = function.MOperations(function.SUB, 1,2,3,4)
+	fmt.Println("multy sub: ", v, "Error: ", err)
+	fmt.Println("End of the program")
 
 
 }
